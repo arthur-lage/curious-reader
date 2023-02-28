691b6732ee86c98d2157db5015efa8ad998c1f2f
Arthur Lage <arthurlage2006@gmail.com>
2023-02-28 16:59:02 -0300

    create user card styles, create animations

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const BASE_URL = "https://curiouscat.live/api/v2.1/profile?username=";
 const usernameInput = document.querySelector("#username");
 const searchButton = document.querySelector("#search");
+const searchForm = document.querySelector(".search");
 
+const loadingSpinner = document.querySelector(".loading");
+
+const userDataWrapper = document.querySelector(".user-data");
 const twitterUsername = document.querySelector(".twitter-username");
 const userBanner = document.querySelector(".user-banner");
 const userAvatar = document.querySelector(".user-avatar");
 const userAnswers = document.querySelector(".user-answers");
 
+let isLoadingData = false;
 let apiData = {};
 
 const renderData = () => {
   if (!apiData) {
     throw new Error("No data was found.");
   }
+
+  userDataWrapper.classList.add("showing");
 
   twitterUsername.innerHTML = apiData.userData.username;
   userBanner.src = apiData.userData.banner;
@@ -22,12 +29,27 @@ const renderData = () => {
 
 const fetchData = async () => {
   const searchedUsername = usernameInput.value;
-  const res = await fetch(`${BASE_URL}${searchedUsername}`);
-  apiData = await res.json();
 
-  renderData();
+  loadingSpinner.classList.add("showing");
+  userDataWrapper.classList.remove("showing");
+
+  try {
+    const res = await fetch(`${BASE_URL}${searchedUsername}`);
+    apiData = await res.json();
+    renderData();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    loadingSpinner.classList.remove("showing");
+  }
 };
 
-searchButton.addEventListener("click", () => {
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
   fetchData();
 });
