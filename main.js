d42356744eb5cb3535047e63e4d80bb9c02b2bae
Arthur Lage <arthurlage2006@gmail.com>
2023-03-03 14:51:33 -0300

    fix searching problems, handle user not found error, use toast to show errors on screen

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const BASE_URL = "https://curiouscat.live/api/v2.1/profile?username=";
 const usernameInput = document.querySelector("#username");
 const searchButton = document.querySelector("#search");
 const searchForm = document.querySelector(".search");
+const questionsAndRepliesWrapper = document.querySelector(
+  ".questions-and-replies-wrapper"
+);
 
 const loadingSpinner = document.querySelector(".loading");
 
@@ -15,8 +18,20 @@ let isLoadingData = false;
 let apiData = {};
 
 const renderData = () => {
-  if (!apiData) {
-    throw new Error("No data was found.");
+  if (!apiData.twitterid) {
+    return Toastify({
+      text: "Could not find user.",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #CC1738, #F33838)",
+        fontSize: "18px",
+      },
+    }).showToast();
   }
 
   userDataWrapper.classList.add("showing");
@@ -25,10 +40,18 @@ const renderData = () => {
   userBanner.src = apiData.userData.banner;
   userAvatar.src = apiData.userData.avatar;
   userAnswers.innerHTML = apiData.userData.answers;
+
+  apiData.posts.forEach((post) => {});
+
+  questionsAndRepliesWrapper.scrollIntoView({
+    behavior: "smooth",
+  });
 };
 
 const fetchData = async () => {
   const searchedUsername = usernameInput.value;
+
+  if (!searchedUsername) return;
 
   loadingSpinner.classList.add("showing");
   userDataWrapper.classList.remove("showing");
@@ -38,6 +61,7 @@ const fetchData = async () => {
     apiData = await res.json();
     renderData();
   } catch (err) {
+    apiData = null;
     console.error(err);
   } finally {
     loadingSpinner.classList.remove("showing");
